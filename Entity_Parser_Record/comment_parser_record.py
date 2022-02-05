570eca89f7f7784bf1d4a0307d53712535d783c8
ARQMath Lab (Clef 2020) <43124939+ARQMath@users.noreply.github.com>
2022-02-05 17:19:46 -0500

    Update comment_parser_record.py

--- Entity_Parser_Record/comment_parser_record.py
+++ Entity_Parser_Record/comment_parser_record.py
@@ -1,5 +1,5 @@
-from xmlr import xmliter
-from ..Entities.Comment import Comment
+import xml.etree.ElementTree as ET
+from Entities.Comment import Comment
 
 
 class CommentParserRecord:
@@ -9,20 +9,23 @@ class CommentParserRecord:
     def __init__(self, xml_comment_file_path):
 
         self.map_of_comments_for_post = {}
-        for attr_dic in xmliter(xml_comment_file_path, 'row'):
-            comment_id = int(attr_dic["@Id"])
-            post_id = int(attr_dic["@PostId"])
-            text = (attr_dic["@Text"])
+        tree = ET.parse(xml_comment_file_path)
+        root = tree.getroot()
+        for child in root:
+            attr_dic = child.attrib
+            comment_id = int(attr_dic["Id"])
+            post_id = int(attr_dic["PostId"])
+            text = (attr_dic["Text"])
             creation_date = None
             score = None
             user_id = None
 
-            if "@Score" in attr_dic:
-                score = int(attr_dic["@Score"])
-            if "@UserId" in attr_dic:
-                user_id = int(attr_dic["@UserId"])
-            if "@CreationDate" in attr_dic:
-                creation_date = (attr_dic["@CreationDate"])
+            if "Score" in attr_dic:
+                score = int(attr_dic["Score"])
+            if "UserId" in attr_dic:
+                user_id = int(attr_dic["UserId"])
+            if "CreationDate" in attr_dic:
+                creation_date = (attr_dic["CreationDate"])
 
             comment = Comment(comment_id, post_id, text, score, user_id, creation_date)
             if post_id in self.map_of_comments_for_post:
