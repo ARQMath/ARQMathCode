5b0f0a32f881c51174b32ef294e80a590203b724
ARQMath Lab (Clef 2020) <43124939+ARQMath@users.noreply.github.com>
2022-02-05 17:21:00 -0500

    Update vote_parser_record.py

--- Entity_Parser_Record/vote_parser_record.py
+++ Entity_Parser_Record/vote_parser_record.py
@@ -1,5 +1,5 @@
-from xmlr import xmliter
-from ..Entities.Vote import Vote
+import xml.etree.ElementTree as ET
+from Entities.Vote import Vote
 
 
 class VoteParserRecord:
@@ -8,19 +8,23 @@ class VoteParserRecord:
     """
     def __init__(self, xml_vote_file_path):
         self.map_of_votes = {}
-        for attr_dic in xmliter(xml_vote_file_path, 'row'):
-            id = int(attr_dic["@Id"])
-            post_id = int(attr_dic["@PostId"])
-            vote_type_id = int(attr_dic["@VoteTypeId"])
+        tree = ET.parse(xml_vote_file_path)
+        root = tree.getroot()
+        for child in root:
+            attr_dic = child.attrib
+            id = int(attr_dic['Id'])
+            post_id = int(attr_dic['PostId'])
+            vote_type_id = int(attr_dic["VoteTypeId"])
             user_id = None
             bounty_amount = None
             creation_date = None
-            if "@UserId" in attr_dic:
-                user_id = int(attr_dic["@UserId"])
-            if "@BountyAmount" in attr_dic:
-                bounty_amount = int(attr_dic["@BountyAmount"])
-            if "@CreationDate" in attr_dic:
-                creation_date = attr_dic["@CreationDate"]
+            if "UserId" in attr_dic:
+                user_id = int(attr_dic["UserId"])
+            if "BountyAmount" in attr_dic:
+                bounty_amount = int(attr_dic["BountyAmount"])
+            if "CreationDate" in attr_dic:
+                creation_date = attr_dic["CreationDate"]
+
             vote = Vote(id, post_id, vote_type_id, user_id, creation_date, bounty_amount)
             if post_id in self.map_of_votes:
                 self.map_of_votes[post_id].append(vote)
