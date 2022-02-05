d90b026bc9309a39896c0e47916b1d186e03ccf0
ARQMath Lab (Clef 2020) <43124939+ARQMath@users.noreply.github.com>
2022-02-05 17:20:08 -0500

    Update post_link_parser_record.py

--- Entity_Parser_Record/post_link_parser_record.py
+++ Entity_Parser_Record/post_link_parser_record.py
@@ -1,17 +1,21 @@
-from xmlr import xmliter
+import xml.etree.ElementTree as ET
 
 
 class PostLinkParserRecord:
     """
         This class is used for reading the post link file. It reads it record by record.
     """
+
     def __init__(self, xml_post_link_file_path):
         self.map_duplicate_posts = {}
         self.map_related_posts = {}
-        for attr_dic in xmliter(xml_post_link_file_path, 'row'):
-            post_id = int(attr_dic["@PostId"])
-            related_post_id = int(attr_dic["@RelatedPostId"])
-            link_type_id = int(attr_dic["@LinkTypeId"])
+        tree = ET.parse(xml_post_link_file_path)
+        root = tree.getroot()
+        for child in root:
+            attr_dic = child.attrib
+            post_id = int(attr_dic['PostId'])
+            related_post_id = int(attr_dic['RelatedPostId'])
+            link_type_id = int(attr_dic["LinkTypeId"])
 
             if link_type_id == 3:  # Duplicate
                 if post_id in self.map_duplicate_posts:
