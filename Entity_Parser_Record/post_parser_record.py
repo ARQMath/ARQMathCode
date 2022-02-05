f5286bac8562082d2be49186efcc26ad609ae43e
ARQMath Lab (Clef 2020) <43124939+ARQMath@users.noreply.github.com>
2022-02-05 17:21:37 -0500

    Update post_parser_record.py

--- Entity_Parser_Record/post_parser_record.py
+++ Entity_Parser_Record/post_parser_record.py
@@ -1,5 +1,5 @@
-from xmlr import xmliter
-from ..Entities.Post import Answer, Question
+import xml.etree.ElementTree as ET
+from Entities.Post import Question, Answer
 
 
 class PostParserRecord:
@@ -7,15 +7,19 @@ class PostParserRecord:
         This class is used for reading the post file. It reads it record by record.
     """
     def __init__(self, xml_post_link_file_path, map_comments=None, map_related_post=None, map_duplicate_post=None,
-                 map_votes=None, map_users=None):
+                 map_votes=None, map_users=None, post_history_parser=None):
         self.map_questions = {}
         self.map_answers = {}
         self.map_just_answers = {}
-        for attr_dic in xmliter(xml_post_link_file_path, 'row'):
-            post_id = int(attr_dic['@Id'])
-            post_type_id = int(attr_dic['@PostTypeId'])
-            creation_date = (attr_dic["@CreationDate"])
-            body = (attr_dic["@Body"])
+        tree = ET.parse(xml_post_link_file_path)
+        root = tree.getroot()
+        for child in root:
+            attr_dic = child.attrib
+            post_id = int(attr_dic['Id'])
+            post_type_id = int(attr_dic['PostTypeId'])
+            creation_date = (attr_dic["CreationDate"])
+            body = (attr_dic["Body"])
+
             view_count = None
             comment_count = None
             owner_user_id = None
@@ -27,36 +31,39 @@ class PostParserRecord:
             score = None
             user = None
 
-            if "@ViewCount" in attr_dic:
-                view_count = int(attr_dic["@ViewCount"])
-            if "@Score" in attr_dic:
-                score = int(attr_dic["@Score"])
-            if "@CommentCount" in attr_dic:
-                comment_count = int(attr_dic["@CommentCount"])
-            if "@OwnerUserId" in attr_dic:
-                owner_user_id = int(attr_dic["@OwnerUserId"])
+            if "ViewCount" in attr_dic:
+                view_count = int(attr_dic["ViewCount"])
+            if "Score" in attr_dic:
+                score = int(attr_dic["Score"])
+            if "CommentCount" in attr_dic:
+                comment_count = int(attr_dic["CommentCount"])
+            if "OwnerUserId" in attr_dic:
+                owner_user_id = int(attr_dic["OwnerUserId"])
                 if map_users is not None and owner_user_id in map_users:
                     user = map_users[owner_user_id]
-            if "@LastEditDate" in attr_dic:
-                last_edit_date = (attr_dic["@LastEditDate"])
-            if "@LastActivityDate" in attr_dic:
-                last_activity_date = (attr_dic["@LastActivityDate"])
-            if "@LastEditorUserId" in attr_dic:
-                last_editor_user_id = int(attr_dic["@LastEditorUserId"])
-            if "@CommunityOwnedDate" in attr_dic:
-                community_owned_date = (attr_dic["@CommunityOwnedDate"])
-            if "@LastEditorDisplayName" in attr_dic:
-                last_editor_display_name = (attr_dic["@LastEditorDisplayName"])
+            if "LastEditDate" in attr_dic:
+                last_edit_date = (attr_dic["LastEditDate"])
+            if "LastActivityDate" in attr_dic:
+                last_activity_date = (attr_dic["LastActivityDate"])
+            if "LastEditorUserId" in attr_dic:
+                last_editor_user_id = int(attr_dic["LastEditorUserId"])
+            if "CommunityOwnedDate" in attr_dic:
+                community_owned_date = (attr_dic["CommunityOwnedDate"])
+            if "LastEditorDisplayName" in attr_dic:
+                last_editor_display_name = (attr_dic["LastEditorDisplayName"])
 
             comment_list = None
             vote_list = None
+            edit_list = None
             if map_comments is not None and post_id in map_comments:
                 comment_list = map_comments[post_id]
             if map_votes is not None and post_id in map_votes:
                 vote_list = map_votes[post_id]
+            if post_history_parser is not None and post_id in post_history_parser.map_of_edits:
+                edit_list = post_history_parser.map_of_edits[post_id]
 
             if post_type_id == 1:  # Question
-                title = (attr_dic["@Title"])
+                title = (attr_dic["Title"])
                 favourite_count = None
                 closed_date = None
                 accepted_answer_id = None
@@ -70,18 +77,18 @@ class PostParserRecord:
                     for related_post_id in map_duplicate_post[post_id]:
                         related_post.append((related_post_id, True))
 
-                if "@CommentCount" in attr_dic:
-                    comment_count = int(attr_dic["@CommentCount"])
-                if "@AnswerCount" in attr_dic:
-                    answer_count = int(attr_dic["@AnswerCount"])
-                if "@FavoriteCount" in attr_dic:
-                    favourite_count = int(attr_dic["@FavoriteCount"])
-                if "@AcceptedAnswerId" in attr_dic:
-                    accepted_answer_id = int(attr_dic["@AcceptedAnswerId"])
-                if "@ClosedDate" in attr_dic:
-                    closed_date = (attr_dic["@ClosedDate"])
-                if "@Tags" in attr_dic:
-                    tags = (attr_dic["@Tags"]).split(">")
+                if "CommentCount" in attr_dic:
+                    comment_count = int(attr_dic["CommentCount"])
+                if "AnswerCount" in attr_dic:
+                    answer_count = int(attr_dic["AnswerCount"])
+                if "FavouriteCount" in attr_dic:
+                    favourite_count = int(attr_dic["FavouriteCount"])
+                if "AcceptedAnswerId" in attr_dic:
+                    accepted_answer_id = int(attr_dic["AcceptedAnswerId"])
+                if "ClosedDate" in attr_dic:
+                    closed_date = (attr_dic["ClosedDate"])
+                if "Tags" in attr_dic:
+                    tags = (attr_dic["Tags"]).split(">")
                     lst_tags = []
                     for i in range(0, len(tags) - 1):
                         tag = tags[i][1:]
@@ -90,14 +97,14 @@ class PostParserRecord:
                                                        comment_count, last_edit_date, last_activity_date,
                                                        last_editor_user_id, community_owned_date,
                                                        last_editor_display_name, related_post, comment_list, vote_list,
-                                                       user, title, lst_tags,
+                                                       edit_list, user, title, lst_tags,
                                                        accepted_answer_id, answer_count, favourite_count, closed_date)
 
             elif post_type_id == 2:
-                parent_id = int(attr_dic["@ParentId"])
+                parent_id = int(attr_dic["ParentId"])
                 answer = Answer(post_id, creation_date, score, view_count, body, owner_user_id, comment_count,
                                 last_edit_date, last_activity_date, last_editor_user_id, community_owned_date,
-                                last_editor_display_name, parent_id, comment_list, vote_list, user)
+                                last_editor_display_name, parent_id, comment_list, vote_list, edit_list, user)
 
                 if parent_id in self.map_answers:
                     self.map_answers[parent_id].append(answer)
