0ad301ed3757bad97a685de52085a3c14069700a
bm3302 <bm3302@rit.edu>
2022-02-10 16:09:00 -0500

    update post_parser_record.py

--- Prepare_Dataset/associate_formula_comment_ids.py
+++ Prepare_Dataset/associate_formula_comment_ids.py
@@ -1,13 +1,27 @@
+"""
+The goal of this code is to associate each formula in the TSV file to its related comment id
+It reads the TSV formula files and select the formulas in the comments.
+In the released versions of ARQMath-1 and -2, the column post id showed what post this formula was located in and
+not exactly which comment. This code creates a TSV file first column showing the formula id and the second column
+showing the comment id.
+"""
+
 import os
 import csv
+import argparse
+import sys
 
 from bs4 import BeautifulSoup
-from os.path import exists
+conf_path = os.getcwd()
+sys.path.append(conf_path)
 from Entity_Parser_Record.comment_parser_record import CommentParserRecord
 from Prepare_Dataset.re_generate_post import match_to_pattern, check_existence
 
 
 def read_formula_file(directory):
+    """
+    This method reads the formula files and returns a dictionary of post id: dictionary of formula id, latex
+    """
     dic_post_id_formula = {}
     counter = 0
     for file in os.listdir(directory):
@@ -26,11 +40,15 @@ def read_formula_file(directory):
                     dic_post_id_formula[post_id][formula_id] = latex
                 else:
                     dic_post_id_formula[post_id] = {formula_id: latex}
-    print(counter)
     return dic_post_id_formula
 
 
 def find_already_assigned_formula_ids(cr):
+    """
+    In the previous XML files released in ARQMath-1 and -2 some formulas were already assigned
+    to comment and were inside a math-container tag. This method finds these formulas and returns
+    dictionary of formula id: comment id
+    """
     dic_formula_comment_id = {}
     for comment_id in cr.map_just_comments:
         comment_body = cr.map_just_comments[comment_id].text
@@ -42,61 +60,63 @@ def find_already_assigned_formula_ids(cr):
     return dic_formula_comment_id
 
 
-def manage_already_associated_formulas(cr, csv_writer):
-    dic_formula_id_comment_id = find_already_assigned_formula_ids(cr)
-    for formula_id in dic_formula_id_comment_id:
-        csv_writer.writerow([formula_id, dic_formula_id_comment_id[formula_id]])
-    return dic_formula_id_comment_id
-
-
 def associate_formula_id_with_comment_id(comment_file_path, directory, accociation_file):
     # this method associate formula ids to comment ids (as tsv file)
-    with open(accociation_file, mode='w') as csv_file:
-        csv_writer = csv.writer(csv_file, delimiter='\t', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    # reading xml comment file
+    cr = CommentParserRecord(comment_file_path)
 
-        # reading xml comment file
-        cr = CommentParserRecord(comment_file_path)
+    # find the formulas that are already assigned to comments (in comment xml file they are in math-container tag with id)
+    dic_formula_id_comment_id = find_already_assigned_formula_ids(cr)
 
-        # find the formulas that are already assigned to comments (in comment xml file they are in math-container tag with id)
-        dic_formula_id_comment_id = manage_already_associated_formulas(cr, csv_writer)
+    # reading formulas ids from tsv; those in comments
+    # dictionary --> post id : list tuples (formula id, latex)
+    dic_post_id_formula = read_formula_file(directory)
 
-        # reading formulas ids from tsv; those in comments
-        # dictionary --> post id : list tuples (formula id, latex)
-        dic_post_id_formula = read_formula_file(directory)
+    not_found_count = 0
+    for post_id in dic_post_id_formula:
+        if post_id not in cr.map_of_comments_for_post:
+            not_found_count += len(dic_post_id_formula[post_id])
+            continue
 
-        not_found_count = 0
-        for post_id in dic_post_id_formula:
-            if post_id not in cr.map_of_comments_for_post:
-                not_found_count += len(dic_post_id_formula[post_id])
+        #sort by length
+        dic_formulas_in_comments = dic_post_id_formula[post_id]
+        sorted_keys = sorted(dic_formulas_in_comments, key=lambda k: len(dic_formulas_in_comments[k]), reverse=True)
+
+        # list comments
+        lst_comments = cr.map_of_comments_for_post[post_id]
+        for formula_id in sorted_keys:
+            # formula already assigned
+            if formula_id in dic_formula_id_comment_id:
                 continue
 
-            #sort by length
-            dic_formulas_in_comments = dic_post_id_formula[post_id]
-            sorted_keys = sorted(dic_formulas_in_comments, key=lambda k: len(dic_formulas_in_comments[k]), reverse=True)
+            latex = dic_formulas_in_comments[formula_id]
+            find = False
+            for comment in lst_comments:
+                if check_existence(match_to_pattern(latex, comment.text)):
+                    dic_formula_id_comment_id[formula_id] = comment.id
+                    comment.text = comment.text.replace(latex, "XFXFX", 1)
+                    find = True
+                    break
+            if not find:
+                not_found_count += 1
 
-            # list comments
-            lst_comments = cr.map_of_comments_for_post[post_id]
-            for formula_id in sorted_keys:
-                # formula already assigned
-                if formula_id in dic_formula_id_comment_id:
-                    continue
-
-                latex = dic_formulas_in_comments[formula_id]
-                find = False
-                for comment in lst_comments:
-                    if check_existence(match_to_pattern(latex, comment.text)):
-                        csv_writer.writerow([formula_id, comment.id])
-                        # dic_formula_id_comment_id[formula_id] = comment.id
-                        comment.text = comment.text.replace(latex, "XFXFX", 1)
-                        find = True
-                        break
-                if not find:
-                    not_found_count += 1
-
-        print(not_found_count)
+    with open(accociation_file, mode='w') as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter='\t', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        for formula_id in dic_formula_id_comment_id:
+            csv_writer.writerow([str(formula_id), str(dic_formula_id_comment_id[formula_id])])
 
 
-comment_file = "/home/bm3302/Comments.V1.2.xml"
-latex_dir = "/home/bm3302/latex_representation_v3/"
-accociation_file = "formula_comment_id.tsv"
-associate_formula_id_with_comment_id(comment_file, latex_dir, accociation_file)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-com', type=str, help='comment file path')
+    parser.add_argument('-ldir', type=str, help='laTex TSV files directory')
+    args = vars(parser.parse_args())
+
+    comment_file = args['com']
+    latex_dir = args['ldir']
+    association_file = "formula_comment_id.tsv"
+    associate_formula_id_with_comment_id(comment_file, latex_dir, association_file)
+
+
+if __name__ == '__main__':
+    main()
